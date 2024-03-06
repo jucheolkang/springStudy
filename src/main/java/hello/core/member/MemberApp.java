05966574cc11e71bf331bef33ddee845a3b90332
jucheolkang <jucheol9877@naver.com>
2024-03-07 01:28:49 +0900

    스프링 핵심 원리 이해2 - 객체 지향 원리 적용 - IoC, DI, 그리고 컨테이너

--- src/main/java/hello/core/member/MemberApp.java
+++ src/main/java/hello/core/member/MemberApp.java
@@ -1,13 +1,19 @@
 package hello.core.member;
 
 import hello.core.order.AppConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+/*        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();*/
+
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class); // config에 있는 환경 설정 정보를 가지고 @Bean으로 등록된 메소드를 객체생성해서 스프링 컨테이너로 관리
+        MemberService memberService = applicationContext.getBean("memberService",MemberService.class);
 //        MemberService memberService = new MemberServiceImpl();
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
--- src/main/java/hello/core/order/AppConfig.java
+++ src/main/java/hello/core/order/AppConfig.java
@@ -7,23 +7,30 @@ import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration // 설정 정보에 붙이는 어노테이션
 public class AppConfig {
 
     // 역할이 들어나도록 수정
 
+    @Bean // @Bean을 붙이면 해당 메소드가 스프링 컨테이너에 등록된다
     public MemberService memberService() {
         return new MemberServiceImpl(MemberRepository());
     }
 
-    private static MemberRepository MemberRepository() {
+    @Bean
+    public static MemberRepository MemberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(MemberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
 //        return new FixDiscountPlicy();
         return new RateDiscountPolicy();
